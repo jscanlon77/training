eef980f8b94908d69374921c2a8f63f92b5e6ff9
jscanlon77 <jscanlon77@googlemail.com>
2019-09-26 12:11:54 +0100

    added the files

---
+++ Structures/LinkedList/DoubleNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Structures.LinkedList
+{
+    public class DoubleNode
+    {
+        internal int data;
+        internal DoubleNode next;
+        internal DoubleNode previous;
+
+        public DoubleNode(int d)
+        {
+            data = d;
+            previous = null;
+            next = null;
+        }
+    }
+}
---
+++ Structures/LinkedList/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Structures.LinkedList
+{
+    /// <summary>
+    /// This node is for a single linked list -= it is a forward only node;
+    /// </summary>
+    internal class Node
+    {
+        internal int data;
+        internal Node next;
+
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+}
